8be9b3b1485673151c202dbd9f89d39c387f933f
akomakom <regs@akom.net>
2017-11-17 20:05:06 -0700

    cert for google docs

--- main/temperature.h
+++ main/temperature.h
@@ -2,11 +2,12 @@
 #define TEMPERATURE_READ_FREQUENCY 60000
 
 /**
- * Reject temperature readings beyond this value, they are bad data
- * With the DS18B20, we seem to be getting a value above 4000
- * when something goes wrong
+ * Reject temperature readings outside said boundaries, they are bad data
+ * With the DS18B20, we seem to be getting a value above 4000 when something
+ * goes wrong, but random values like 270C were also observed on occasion.
+ * This is in Celsius.
  * **/
-#define TEMPERATURE_ACCEPTABLE_MAX 1000
-#define TEMPERATURE_ACCEPTABLE_MIN -1000
+#define TEMPERATURE_ACCEPTABLE_MAX 200
+#define TEMPERATURE_ACCEPTABLE_MIN -100
 
 void initialize_temperature(void (*callback)(float));
